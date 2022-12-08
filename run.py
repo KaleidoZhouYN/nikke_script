490170811c348452e67acfa689534922fe270fda
yinan.zhou <zhouyinan1994@gmail.com>
2022-12-08 23:53:42 +0800

    add qt_adaptive component size

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-from PyQt5.QtWidgets import (QWidget, QLabel, 
-    QComboBox, QApplication,
-    QHBoxLayout,QVBoxLayout,
-    QMessageBox,QPushButton,
-    QCheckBox)
+from PyQt5.QtWidgets import (QWidget,  QApplication,
+    QMessageBox,QPushButton, QLabel, QComboBox, QCheckBox,
+    QSizePolicy)
+
 import sys
 import types
 import win32gui
@@ -24,11 +23,44 @@ class Nikke_Toolkit(QWidget):
     def __init__(self):
         super().__init__()
         self.keyboard = Controller()
+        self.components = []
+        self.setGeometry(300, 300, 400, 200)
+        self.setWindowTitle('NIKKE_TOOLKIT')
+
+        oldsize = self.size()
+        self.oldsize = [oldsize.width(),oldsize.height()]
         self.initUI()
+
+
+    def regist(self,com):
+        # 适应字体大小
+        com.adjustSize()
+
+        # 记录下原来的rect大小并加入components组件中
+        oldrect = com.geometry()
+        com.oldrect = [oldrect.left(),oldrect.top(),oldrect.width(),oldrect.height()]
+        self.components.append(com)
+
+    def resizeEvent(self, a0) -> None:
+        super().resizeEvent(a0)
+        
+        # 根据放缩的factor来对每个组件的rect进行调整
+        newsize = self.size()
+        newsize = [newsize.width(),newsize.height()]
+        factor = [newsize[0]/self.oldsize[0], newsize[1]/self.oldsize[1]]
+        for com in self.components:
+            newrect = [com.oldrect[0]*factor[0], 
+                        com.oldrect[1]*factor[1],
+                        com.oldrect[2]*factor[0],
+                        com.oldrect[3]*factor[1]]
+            newrect = [int(c) for c in newrect]
+            com.setGeometry(*newrect)
+        
 
     def add_active_window(self):
         text = QLabel("请选择你的模拟器窗口,如果没有找到，\n请打开模拟器并重新点击下拉框",self)
         text.move(50,5)
+        self.regist(text)
         self.active_window_combo = QComboBox(self)
         active_window_title = get_active_window()[0]
         for a in active_window_title:
@@ -44,6 +76,7 @@ class Nikke_Toolkit(QWidget):
         self.active_window_combo.move(50,40)
         self.active_window_combo.showPopup = types.MethodType(showPopup,self.active_window_combo)
         self.active_window_combo.resize(300,20)
+        self.regist(self.active_window_combo)
 
         def onActivated(text):
             self.select_title = text
@@ -66,6 +99,7 @@ class Nikke_Toolkit(QWidget):
     def add_screenshot(self):
         btn1 = QPushButton("点击此处测试模拟器截图",self)
         btn1.move(50,70)
+        self.regist(btn1)
 
         def screenshot():
             img = get_screenshot_by_hwnd(self.simulator_hWnd,0,1)
@@ -95,9 +129,15 @@ class Nikke_Toolkit(QWidget):
         self.battle_s_process = None
         text = QLabel("拦截S自动瞄准,按f可以进入/解除防御状态",self)
         text.move(50,100)
-        btn1 = QPushButton("开始",self)
-        btn2 = QPushButton("结束",self)
+        self.regist(text)
 
+        btn1 = QPushButton("开始",self)
+        btn1.move(50,120)
+        self.regist(btn1)
+
+        btn2 = QPushButton("结束",self)
+        btn2.move(200,120)
+        self.regist(btn2)
 
         # refer: https://stackoverflow.com/questions/4789837/how-to-terminate-a-python-subprocess-launched-with-shell-true
         def start_battle_s():
@@ -118,14 +158,16 @@ class Nikke_Toolkit(QWidget):
         btn1.clicked.connect(start_battle_s)
         btn2.clicked.connect(end_battle_s)
 
-        btn1.move(50,120)
-        btn2.move(200,120)
+        
+        
 
         drill = QCheckBox('优先瞄准钻头(未实现）',self)
         drill.move(50,150)
+        self.regist(drill)
 
         defend = QCheckBox('自动防御(未实现)',self)
         defend.move(200,150)
+        self.regist(defend)
 
 
 
@@ -133,9 +175,7 @@ class Nikke_Toolkit(QWidget):
         self.add_active_window()
         self.add_screenshot()
         self.add_battle_S()
-
-        self.setGeometry(300, 300, 400, 200)
-        self.setWindowTitle('NIKKE_TOOLKIT')
+        
         self.show()
 
 
